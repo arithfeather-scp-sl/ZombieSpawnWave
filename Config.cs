c95a91415a7e9badeca7bc87c0db988a0a7845ef
MOBILEVOYAGER\Arith <dustswier@gmail.com>
2020-08-10 04:32:53 -0600

    Exiled 2 build (v2.0) WIP

---
+++ Config.cs
@@ -0,0 +1,12 @@
+﻿using Exiled.API.Interfaces;
+
+namespace ArithFeather.ZombieSpawnWave {
+	public class Config : IConfig
+	{
+		public bool IsEnabled { get; set; } = true;
+
+		public int PercentChanceToSpawnZombies { get; set; } = 100;
+
+		public string ZombieAnnounceMessage { get; set; }= "SCP 0 4 9 2 CONTAINMENT BREACH IN ENTRANCE ZONE";
+	}
+}
--- ZombieSpawnWave.cs
+++ ZombieSpawnWave.cs
@@ -1,108 +1,68 @@
-﻿using ArithFeather.ArithSpawningKit.RandomPlayerSpawning;
-using ArithFeather.ArithSpawningKit.SpawnPointTools;
-using Smod2;
-using Smod2.API;
-using Smod2.Attributes;
-using Smod2.Config;
-using Smod2.EventHandlers;
-using Smod2.Events;
-using Smod2.EventSystem.Events;
+﻿using System;
 using System.Collections.Generic;
+using ArithFeather.AriToolKit;
+using ArithFeather.AriToolKit.PointEditor;
+using Exiled.API.Features;
+using SpawnPoint = ArithFeather.AriToolKit.PointEditor.FixedPoint;
 
-namespace ArithFeather.ZombieSpawnWave
-{
-	[PluginDetails(
-		author = "Arith",
-		name = "Zombie Spawn Wave",
-		description = "",
-		id = "ArithFeather.ZombieSpawnWave",
-		configPrefix = "zsw",
-		version = "1.0",
-		SmodMajor = 3,
-		SmodMinor = 4,
-		SmodRevision = 0
-		)]
-	public class ZombieSpawnWave : Plugin, IEventHandlerWaitingForPlayers, IEventHandlerTeamRespawn
-	{
-		public override void Register()
-		{
-			AddEventHandlers(this);
-		}
-		public override void OnEnable() => Info("ZombieSpawnWave Enabled");
-		public override void OnDisable() => Info("ZombieSpawnWave Disabled");
+namespace ArithFeather.ZombieSpawnWave {
+	public class ZombieSpawnWave : Plugin<Config> {
+		private const string SpawnPointFIleName = "ZombieSpawns";
 
-		[ConfigOption] private readonly bool disablePlugin = false;
-		[ConfigOption] private readonly int percentChanceToSpawnZombies = 20;
-		[ConfigOption] private readonly string zombieAnnounceMessage = "SCP 0 4 9 2 CONTAINMENT BREACH IN ENTRANCE ZONE";
+		public override string Author => "Arith";
+		public override Version Version => new Version("2.0");
 
-		private List<SpawnPoint> zombieSpawns;
-		public List<SpawnPoint> ZombieSpawns => zombieSpawns ?? (zombieSpawns = new List<SpawnPoint>());
+		public override void OnEnabled() {
+			base.OnEnabled();
 
-		private List<PlayerSpawnPoint> loadedZombieSpawns;
-		public List<PlayerSpawnPoint> LoadedZombieSpawns => loadedZombieSpawns ?? (loadedZombieSpawns = new List<PlayerSpawnPoint>());
-
-		private bool useZombies = false;
-		
-		public void OnWaitingForPlayers(WaitingForPlayersEvent ev)
-		{
-			if (disablePlugin)
-			{
-				PluginManager.DisablePlugin(this);
-				return;
-			}
-
-			zombieSpawns = SpawnDataIO.Open("sm_plugins/ZombieSpawns.txt");
-
-			LoadedZombieSpawns.Clear();
-
-			var playerPointCount = ZombieSpawns.Count;
-			var rooms = CustomRoomManager.Instance.Rooms;
-			var roomCount = rooms.Count;
-
-			// Create player spawn points on map
-			for (var i = 0; i < roomCount; i++)
-			{
-				var r = rooms[i];
-
-				for (var j = 0; j < playerPointCount; j++)
-				{
-					var p = ZombieSpawns[j];
-
-					if (p.RoomType == r.Name && p.ZoneType == r.Zone)
-					{
-						LoadedZombieSpawns.Add(new PlayerSpawnPoint(p.RoomType, p.ZoneType,
-							Tools.Vec3ToVec(r.Transform.TransformPoint(Tools.VecToVec3(p.Position))) + new Vector(0, 0.3f, 0),
-							Tools.Vec3ToVec(r.Transform.TransformDirection(Tools.VecToVec3(p.Rotation)))));
-					}
-				}
-			}
-
-			useZombies = loadedZombieSpawns.Count > 0;
-			if (!useZombies) Info("There are no zombie spawn points in this map seed.");
+			Exiled.Events.Handlers.Server.ReloadedConfigs += Server_ReloadedConfigs;
+			Exiled.Events.Handlers.Server.WaitingForPlayers += Server_WaitingForPlayers;
+			Exiled.Events.Handlers.Server.RespawningTeam += Server_RespawningTeam;
+			Exiled.Events.Handlers.Warhead.Detonated += Warhead_Detonated;
 		}
 
-		public void OnTeamRespawn(TeamRespawnEvent ev)
-		{
-			if (useZombies && UnityEngine.Random.Range(0, 101) <= percentChanceToSpawnZombies)
-			{
-				if (!string.IsNullOrWhiteSpace(zombieAnnounceMessage)) Server.Map.AnnounceCustomMessage(zombieAnnounceMessage);
+		public override void OnDisabled() {
+			Exiled.Events.Handlers.Server.ReloadedConfigs -= Server_ReloadedConfigs;
+			Exiled.Events.Handlers.Server.WaitingForPlayers -= Server_WaitingForPlayers;
+			Exiled.Events.Handlers.Server.RespawningTeam -= Server_RespawningTeam;
+			Exiled.Events.Handlers.Warhead.Detonated -= Warhead_Detonated;
 
-				LoadedZombieSpawns.Shuffle();
+			base.OnDisabled();
+		}
+
+		private void Server_ReloadedConfigs() => _spawnPointFile = PointAPI.GetPointList(SpawnPointFIleName);
+
+		private PointList _spawnPointFile = PointAPI.GetPointList(SpawnPointFIleName);
+		private bool _useZombies;
+
+		private List<SpawnPoint> LoadedZombieSpawns => _spawnPointFile.FixedPoints;
+
+		private void Server_WaitingForPlayers() {
+			_useZombies = LoadedZombieSpawns.Count > 0;
+			if (!_useZombies) Log.Warn("There are no zombie spawn points.");
+			Log.Error(_useZombies);
+		}
+
+		private void Server_RespawningTeam(Exiled.Events.EventArgs.RespawningTeamEventArgs ev) {
+			if (_useZombies && UnityEngine.Random.Range(0, 101) <= Config.PercentChanceToSpawnZombies) {
+				Log.Error("Spawn zombs");
+				if (!string.IsNullOrWhiteSpace(Config.ZombieAnnounceMessage))
+					Cassie.Message(Config.ZombieAnnounceMessage);
+
+				LoadedZombieSpawns.UnityShuffle();
 
 				var index = 0;
 				var LoadedSpawnsCount = LoadedZombieSpawns.Count;
 
-				var spawns = ev.PlayerList;
+				var spawns = ev.Players;
 				var spawnCount = spawns.Count;
-				for (int i = 0; i < spawnCount; i++)
-				{
+				for (int i = 0; i < spawnCount; i++) {
 					var spawn = spawns[i];
-					spawn.ChangeRole(Role.SCP_049_2, true, false);
-					spawn.Teleport(LoadedZombieSpawns[index].Position);
+					spawn.SetRole(RoleType.Scp0492);
+					//spawn.Position = LoadedZombieSpawns[index].Position;
 
 					index++;
-					if (index == LoadedSpawnsCount)
-					{
+					if (index == LoadedSpawnsCount) {
 						index = 0;
 					}
 				}
@@ -110,5 +70,7 @@ namespace ArithFeather.ZombieSpawnWave
 				spawns.Clear();
 			}
 		}
+
+		private void Warhead_Detonated() => _useZombies = false;
 	}
 }
